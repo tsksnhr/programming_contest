30c08c9a3f5dbfa7e653e19b90187cce8d9319fc
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-04-29 23:11:39 +0900

    edited file
    to solve problem

--- abc162d.cpp
+++ abc162d.cpp
@@ -3,33 +3,73 @@ using namespace std;
 
 int main(){
 
-    int input_num;
-    cin >> input_num;
+    int N;
+    string S;
+    cin >> N >> S;
 
-    string str;
-    cin >> str;
+    vector<int> sum_R(N+1); 
+    vector<int> sum_G(N+1);
+    vector<int> sum_B(N+1);
 
-    int count_i, count_j, count_k;
-    char si, sj, sk;
-    int ans = 0;
+    int i, j, temp;
 
-    for (count_i=0; count_i < input_num; count_i++){
-        for (count_j = count_i + 1; count_j < input_num; count_j++){
-            for (count_k = count_j + 1; (count_k < input_num) /*&& ((count_j - count_i) != (count_k - count_j))*/; count_k++){
-//                count_k = input_num - count_i - count_j;
-                si = str.at(count_i);
-                sj = str.at(count_j);
-                sk = str.at(count_k);
+    //  累積和  
+    for (i=0; i<N; i++){
+        if (S.at(i) == 'R'){
+            sum_R.at(i+1) = 1 + sum_R.at(i);
+            sum_G.at(i+1) = sum_G.at(i);
+            sum_B.at(i+1) = sum_B.at(i);
+        }
+        if (S.at(i) == 'G'){
+            sum_R.at(i+1) = sum_R.at(i);
+            sum_G.at(i+1) = 1 + sum_G.at(i);
+            sum_B.at(i+1) = sum_B.at(i);
+        }
+        if (S.at(i) == 'B'){
+            sum_R.at(i+1) = sum_R.at(i);
+            sum_G.at(i+1) = sum_G.at(i);
+            sum_B.at(i+1) = 1 + sum_B.at(i);
+        }
+    }
 
-                if ((si != sj) && (sj != sk) && (sk != si)){
-                    if ((count_j - count_i) != (count_k - count_j)){
-                        ans++;
-                    }
+    int64_t count = 0;  // 組み合わせ数が多くなった場合オーバーフローするので、それを防ぐためにint64_t型を用いる
+    int diff = 0;
+
+    for (i=0; i<N; i++){
+        for (j=i+1; j<N; j++){
+            temp = 0;
+            diff = j - i;
+            if ( (S.at(i) == 'G' && S.at(j) == 'B') || (S.at(i) == 'B' && S.at(j) == 'G') ){
+                temp += (sum_R.at(N) - sum_R.at(j+1));
+                if ( ((j+diff)<N) && (S.at(j+diff) == 'R') ){
+                    count += temp-1;
+                }
+                else{
+                    count += temp;
+                }
+            }
+            if ( (S.at(i) == 'R' && S.at(j) == 'B') || (S.at(i) == 'B' && S.at(j) == 'R') ){
+                temp += (sum_G.at(N) - sum_G.at(j+1));
+                if ( ((j+diff)<N) && (S.at(j+diff) == 'G') ){
+                    count += temp-1;
+                }
+                else{
+                    count += temp;
+                }
+            }
+            if ( (S.at(i) == 'G' && S.at(j) == 'R') || (S.at(i) == 'R' && S.at(j) == 'G') ){
+                temp += (sum_B.at(N) - sum_B.at(j+1));
+                if ( ((j+diff)<N) && (S.at(j+diff) == 'B') ){
+                    count += temp-1;
+                }
+                else{
+                    count += temp;
                 }
             }
         }
     }
 
-    cout << ans << endl;
+    cout << count << endl;
+
     return 0;
 }
