2654df0e206cb88d44a358fbd84c475d11e1866b
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-06 03:05:52 +0900

    edited file
    to check my understanding

--- test.cpp
+++ test.cpp
@@ -1,15 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int64_t modpow(int64_t a, int64_t dim, int64_t div);
+
 int main(){
 
-    int count_true=0, count_false=0;
+    int a, dim, div, ans;
+    cin >> a >> dim;
+    div = 1e9 + 7;
 
-    bitset<8> a("01010101");
+    ans = modpow(a, dim, div);
 
-    a <<= 1;
-
-    cout << a << endl;
+    cout << ans << endl;
 
     return 0;
+}
+
+//  繰り返し2乗法
+int64_t modpow(int64_t a, int64_t dim, int64_t div){
+
+    if (dim == 1){
+        return a%div; 
+    }
+    else if (dim%2 != 0){
+        return ( a*modpow(a, dim-1, div) )%div;
+    }
+    else{
+        return ( modpow(a, dim/2, div) * modpow(a, dim/2, div) )%div;
+    }
 }
