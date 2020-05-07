2f8821a31358263c6f02710856c0e8a0d4522843
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-07 14:21:40 +0900

    BFSを実装
    最短経路問題を解くため

--- abc151d.cpp
+++ abc151d.cpp
@@ -1,65 +1,79 @@
+//　幅優先探索
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> P;
 
-void DFS(int i, int j, vector<vector<char>> &area, int ans);
+int bfs(int x, int y);
+
+const int INF = 1e6;
+
+int H, W;
+vector<vector<char>> area(20, vector<char>(20));
+
+vector<int> dx = {1, -1, 0, 0};
+vector<int> dy = {0, 0, 1, -1};
+vector<vector<int>> st_distance(20, vector<int>(20));
+int max_distance_s = 0;
+int max_distance_g = 0;
+int max_tmp = 0;
 
 int main(){
 
-    int H, W;
     cin >> H >> W;
-
-    int a=0, b=0, c=0, d=0, max=0;
-    vector<vector<char>> area(H, vector<char>(W));
-
     for (int i=0; i<H; i++){
         for (int j=0; j<W; j++){
             cin >> area.at(i).at(j);
         }
     }
 
-    DFS(0, 0, area, a);
-    DFS(0, W-1, area, b);
-    DFS(H-1, 0, area, c);
-    DFS(H-1, W-1, area, d);
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            max_tmp = bfs(i, j);
 
-    if (max<a){
-        max = a;
-        if (max<b){
-            max = b;
-            if (max<c){
-                max = c;
-                if (max<d){
-                    max = d;
+            if (max_tmp > max_distance_g){
+                max_distance_g = max_tmp;
+            }
+        }
+    }
+
+    cout << max_distance_g << endl;
+    return 0;
+}
+
+int bfs(int x, int y){
+
+    queue<pair<int, int>> q;
+    P p_tmp;
+
+    for (int i=0; i<H; i++){
+        for (int j=0; j<W; j++){
+            st_distance.at(i).at(j) = INF;
+        }
+    }
+
+    st_distance.at(x).at(y) = 0;
+    q.push(P(x, y));
+
+    while (q.size()){
+        p_tmp = q.front();
+        q.pop();
+
+        for (int i=0; i<4; i++){
+            int nx, ny;
+            nx = p_tmp.first + dx.at(i);
+            ny = p_tmp.second + dy.at(i);
+            
+            if ((0<=nx) && (nx<H) && (0<=ny) && (ny<W) && (st_distance.at(nx).at(ny) == INF) && (area.at(nx).at(ny) == '.') && (area.at(p_tmp.first).at(p_tmp.second) == '.')){
+                q.push(P(nx, ny));
+                st_distance.at(nx).at(ny) = st_distance.at(p_tmp.first).at(p_tmp.second) + 1;
+
+                if (st_distance.at(nx).at(ny) > max_distance_s){
+                    max_distance_s = st_distance.at(nx).at(ny);
                 }
             }
         }
     }
 
-    cout << max << endl;
-    return 0;
-}
-
-void DFS(int i, int j, vector<vector<char>> &area, int ans){
-
-    if((i==0) && (j==0) && (&area.at(i).at(j) == ".")){
-        ans++;
-        return;
-    }
-    if ( (i==0) && (&area.at(i).at(j) == ".")){
-        ans++;
-        DFS(i, j-1, area, ans);
-    }
-    if ((j==0) && (&area.at(i).at(j) == ".")){
-        ans++;
-        DFS(i-1, j, area, ans);
-    }
-    if (&area.at(i).at(j) == "#"){
-        return;
-    }
-
-    DFS(i-1, j, area, ans);
-    DFS(i, j-1, area, ans);
-    DFS(i+1, j, area, ans);
-    DFS(i, j+1, area, ans);
-
+    return max_distance_s;
 }
