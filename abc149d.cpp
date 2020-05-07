1154a10ad70573e503c3ab4369c91754363938d1
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-08 02:34:56 +0900

    コメントを追記

--- abc149d.cpp
+++ abc149d.cpp
@@ -33,41 +33,43 @@ int main(){
         else{
             //  t.at(i) == 'r'
             if ((i+k>=n) && (t.at(i) == 'r') && (t_alt.at(i-k) == 'p')){
-                t_alt.push_back('r');
+                t_alt.push_back('s');   //  p以外であればOK
             }
             else if ((i+k<n) && (t.at(i) == 'r') && (t_alt.at(i-k) == 'p') && (t.at(i+k) == 'r')){
-                t_alt.push_back('r');
+                t_alt.push_back('s');   //  p以外であればOK
             }
             else if ((i+k<n) && (t.at(i) == 'r') && (t_alt.at(i-k) == 'p') && (t.at(i+k) == 's')){
-                t_alt.push_back('s');
+                t_alt.push_back('s');   //  rとpはダメ
             }
             else if (t.at(i) == 'r'){
                 t_alt.push_back('p');
                 sum += p;
             }
+
             //  t.at(i) == 's'
             else if ((i+k>=n) && (t.at(i) == 's') && (t_alt.at(i-k) == 'r')){
-                t_alt.push_back('s');
+                t_alt.push_back('p');   //  r以外であればOK
             }
             else if ((i+k<n) && (t.at(i) == 's') && (t_alt.at(i-k) == 'r') && (t.at(i+k) == 's')){
-                t_alt.push_back('p');
+                t_alt.push_back('p');   //  r以外であればOK
             }
             else if ((i+k<n) && (t.at(i) == 's') && (t_alt.at(i-k) == 'r') && (t.at(i+k) == 'p')){
-                t_alt.push_back('p');
+                t_alt.push_back('p');   //  rとsはダメ
             }
             else if (t.at(i) == 's'){
                 t_alt.push_back('r');
                 sum += r;
             }
+
             //  t.at(i) == 'p'
             else if ((i+k>=n) && (t.at(i) == 'p') && (t_alt.at(i-k) == 's')){
-                t_alt.push_back('p');
+                t_alt.push_back('r');   //  sはダメ
             }
             else if ((i+k<n) && (t.at(i) == 'p') && (t_alt.at(i-k) == 's') && (t.at(i+k) == 'r')){
-                t_alt.push_back('r');
+                t_alt.push_back('r');   //  pとsはダメ
             }
             else if ((i+k<n) && (t.at(i) == 'p') && (t_alt.at(i-k) == 's') && (t.at(i+k) == 'p')){
-                t_alt.push_back('r');
+                t_alt.push_back('r');   //  sはダメ
             }
             else if (t.at(i) == 'p'){
                 t_alt.push_back('s');
@@ -78,7 +80,7 @@ int main(){
         }
     }
 
-//    cout << t_alt << endl;
+    cout << t_alt << endl;
     cout << sum << endl;
     return 0;
 }
