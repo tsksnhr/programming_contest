8f6f925b18f978125d0ba895a24503e4f7fcd7dc
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-09 23:59:45 +0900

    ファイルを作成
    全探索の問題を解くため
    (素因数分解による約数全列挙してからの約数計算)

---
+++ abc106b.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void prime_div(int num);
+
+int N;
+vector<int> used(250);
+
+int main(){
+
+    cin >> N;
+
+    int i, j, sum, cnt = 0;
+    for (i=1; i<=N; i += 2){
+        sum = 1;
+        for (int &x: used){     //  範囲for文による要素の初期化は参照を渡す必要がある
+            x = 0;
+        }
+
+        prime_div(i);
+//        cout << "i = " << i << endl;
+/*
+        for (int x: used){
+            cout << x << " ";
+        }
+        cout << endl;
+*/
+        for (j=0; j<201; j++){
+            if (used.at(j) != 0){
+//                cout << "used.at(" << j << ") = " << used.at(j) << endl;
+                sum *= (used.at(j) + 1);
+            }
+        }
+//        cout << "sum = " << sum << endl; 
+        if (sum == 8){
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+    return 0;
+}
+
+void prime_div(int num){
+
+    int div;
+    for (div=2; div<=num; div++){
+        while ((num%div==0) && (num != 0)){
+            num /= div;
+            used.at(div)++;
+//            cout << "num = " << num << " div = " << div << endl;
+        }
+    }
+}
