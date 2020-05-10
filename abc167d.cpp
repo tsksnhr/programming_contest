d83964908e16fe5f07681d82fe82b8e9b81b7eec
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-11 00:36:22 +0900

    未使用変数を削除

--- abc167d.cpp
+++ abc167d.cpp
@@ -13,40 +13,27 @@ int main(){
         cin >> A.at(i);
     }
 
-    long long pos; // = A.at(1);
-    long long move = 0;
+    long long pos = 0;
     long long rmove = 0;
     long long last_move = 0;
-    long long ans = 0;
-    long long ans_pos = 0;
-    long long rstart = 0;
-    long long fmove = 0;
-    bool fflg = false;
 
     pos = 1;
     Acnt.at(1) = 1;
     while (1){
         pos = A.at(pos);
         Acnt.at(pos)++;
-        move++;
         if (Acnt.at(pos) == 3){
-            rstart = pos;
             break;
         }
     }
 
-    for (int i=1; i<N+1; i++){
-        if (Acnt.at(i)==1){
-            fmove++;
-        }
-    }
     for (int i=1; i<N+1; i++){
         if (Acnt.at(i)>1){
             rmove++;
         }
     }
 
-    last_move = (K)%rmove;
+    last_move = K%rmove;
 
     pos = 1;
     for (int i=0; i<last_move; i++){
