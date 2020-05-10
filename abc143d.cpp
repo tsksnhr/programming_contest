12a9325c9fa10a9089d5c92006265db32b732ec4
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-11 01:39:54 +0900

    ファイルを作成(未完)

---
+++ abc143d.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> L(2000);
+vector<vector<int>> pvec;
+vector<int> p(2000);
+vector<int> used(2000);
+
+int main(){
+
+    cin >> n;
+    int i;
+    for (i=0; i<n; i++){
+        cin >> L.at(i);
+    }
+
+
+
+
+}
+
+void permutation(int pos, int n_num){
+
+    if (pos == n_num){
+        return;
+    }
+
+    int i;
+    for (i=1; i<=n_num; i++){
+
+    }
+}
