77e0e68b568c67e6c439b7dcb0c959332235bc61
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-11 01:14:05 +0900

    ループ開始位置とループ前の移動回数を記録するよう変更

--- abc167d.cpp
+++ abc167d.cpp
@@ -14,6 +14,8 @@ int main(){
     }
 
     long long pos = 0;
+    long long rpos = 0;
+    long long fmove = 0;
     long long rmove = 0;
     long long last_move = 0;
 
@@ -23,19 +25,26 @@ int main(){
         pos = A.at(pos);
         Acnt.at(pos)++;
         if (Acnt.at(pos) == 3){
+            rpos = pos;
             break;
         }
     }
 
+    for (int i=1; i<N+1; i++){
+        if (Acnt.at(i)==1){
+            fmove++;
+        }
+    }
     for (int i=1; i<N+1; i++){
         if (Acnt.at(i)>1){
             rmove++;
         }
     }
 
-    last_move = K%rmove;
+    //  ループに入るまでの移動回数がループより大きい場合、ここで引かないとNG
+    last_move = (K-fmove)%rmove;
 
-    pos = 1;
+    pos = rpos;
     for (int i=0; i<last_move; i++){
         pos = A.at(pos);
     }
