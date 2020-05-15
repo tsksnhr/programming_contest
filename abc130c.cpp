7943bd0c68096c065282760cb6645c6ec2006b0d
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-05-16 00:55:16 +0900

    別解法を実装

--- abc130c.cpp
+++ abc130c.cpp
@@ -1,9 +1,19 @@
+//  2分割した面積の小さいほうの最大値は、全面積の半分(長方形に切らなくてもいいので、必ずこうなる切り方がある)
+//  指定の点が中心でない場合、線の引き方は一意に定まる
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main(){
 
-    int w, h, x, y;
+    double w, h, x, y;
     cin >> w >> h >> x >> y;
 
+    double ans = (w*h)/2.0;
+    bool flg = (2.0*x == w) && (2.0*y == h);
+
+    cout << fixed << setprecision(10);
+    cout << ans << " " << flg << endl;
+    return 0;
 }
