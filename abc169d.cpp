4b9fa74a73351003266580b3aac8fee8657b3478
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-06-02 00:08:48 +0900

    implemented prime factrization (not finished)

--- abc169d.cpp
+++ abc169d.cpp
@@ -1,54 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int INF = 1e9;
-
-void is_prime(ll num, vector<ll> &array1, vector<ll> &array2);
 
 int main(){
 
-    ll n;
-    cin >> n;
-    vector<ll> prime_flg(n, 1);
-    vector<ll> prime;
+    ll N;
+    cin >> N;
 
-    is_prime(n, prime_flg, prime);
-    
-    for (auto x: prime){
-        cout << x << endl;
-    }
-    return 0;
-}
+    ll buf = N;
+    map<ll, ll> divisor;
+    ll div = 2;
 
-void is_prime(ll num, vector<ll> &array1, vector<ll> &array2){
-
-    bool flg = false;
-
-    ll pos = 2;
-    array2.push_back(pos);
-    ll index, i = 1;
-    while (pos <= sqrt(num)){
-        index = pos*i;
-
-        if (index > array1.size()){
-            i=0;
-            for (int i=pos; i<array1.size(); i++){
-                if (array1.at(i) == 1){
-                    pos = i;
-                    array2.push_back(pos);
-                    flg = true;
-                    break;
-                }
+    if (buf == 1) divisor[0] = 1;
+    else{
+        while (buf != 1){
+            while (buf%div == 0){
+                buf /= div;
+                divisor[div-1]++;
             }
+            div++;
         }
-
-        if (array1.at(index) == 1){
-            array1.at(index) = 0;
-        }
-        i++;
-
-
     }
 
-    return;
+    ll ans = 0;
+    for (auto p: divisor){
+        auto value = p.second;
+
+        int sub = 1;
+        while (value != 0){
+            value -= sub;
+            sub++;
+            ans++;
+            
+            if (sub > value) break; 
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
 }
