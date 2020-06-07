092190ddc3f21b3607ef944c38aeab568b224c2e
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-06-07 20:02:37 +0900

    implemented triple-roop

--- abc087b.cpp
+++ abc087b.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(int a, int b, int c, int x, int sum);
-
 int A, B, C, X;
 int sum, ans;
 
@@ -12,24 +10,16 @@ int main(){
     sum = 0;
     ans = 0;
 
-    dfs(0, 0, 0, X, sum);
+    for (int i=0; i<A+1; i++){
+        for (int j=0; j<B+1; j++){
+            for (int k=0; k<C+1; k++){
+                sum = i*500 + j*100 + k*50;
+                if (sum == X) ans++;
+                sum = 0;
+            }
+        }
+    }
 
     cout << ans << endl;
     return 0;
-}
-
-void dfs(int a, int b, int c, int x, int sum){
-
-    if (sum == x){
-        ans++;
-        return;
-    }
-    else if (a == A) return;
-    else if (b == B) return;
-    else if (c == C) return;
-    else{
-        dfs(a+1, b, c, x, sum+500);
-        dfs(a, b+1, c, x, sum+100);
-        dfs(a, b, c+1, x, sum+50);
-    }
 }
