d23fe2c89328e4ce8297cf87250ab0f7170684a6
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-06-14 11:55:12 +0900

    claasified in terms of whether "string" or "char"

--- agc037a.cpp
+++ agc037a.cpp
@@ -6,44 +6,25 @@ int main(){
     string s;
     cin >> s;
 
-    int num, pos;
-    num = s.size();
-    pos = 0;
-    map<char, int> cdict;
-    map<string, int> sdict;
+    int num = s.size();
+    int ans = 0;
+    char cbuf, cmem;
 
-    while(pos < num){
-        int i = 0;
-        if (cdict.count(s.at(pos)) == 0){
-            cdict[s.at(pos)]++;
-            cout << "a" << pos << endl;
-        }
-        else {
-            i = 1; 
-            bool flg = false;
-            string buf = s.at(pos) + "";
-            while (flg == false){
-                pos = min(pos+i, num-1);
-            cout << "b" << pos << endl;
-                buf += s.at(pos);
-                if (sdict.count(buf) == 0){
-                    sdict[buf]++;
-                    flg = true;
-                }
-                else i++;
+    for (int i=0; i<num; i++){
+
+        cbuf = s.at(i);
+
+        if (cbuf == cmem){
+            if (i+1<num){
+                ans++;
+                cmem = ' ';
+                i++;
             }
         }
-        pos++;
-        cout << "c" << pos << endl;
-       
-    }
-
-    int ans = 0;
-    for (auto p: cdict){
-        ans++;
-    }
-    for (auto p: sdict){
-        ans++;
+        else {
+            cmem = cbuf;
+            ans++;
+        }
     }
 
     cout << ans << endl;
