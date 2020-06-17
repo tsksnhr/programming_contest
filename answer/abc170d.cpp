00a687ba833af44e762ee74247a5f97373edfe40
tsksnhr <toshiki.o3o5.s@gmail.com>
2020-06-18 00:14:48 +0900

    used my libraly (not finished)
    solved past tests

--- answer/abc170d.cpp
+++ answer/abc170d.cpp
@@ -3,62 +3,81 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-int ruijo(int a, int b);
-ll ruijo(ll a, ll b);
 
+//  libraly
+void prime_factorization(int buf, map<int, int> &divisor){
+
+    int div;
+    int thr = sqrt(buf);
+
+    if (buf == 1) {
+        divisor[0]++;
+        return;
+    }
+    else {
+        for (div=2; div<thr+1; div++){
+            while (buf%div == 0){
+                buf /= div;
+                divisor[div]++;
+            }
+        }
+
+        if (buf != 1) {
+            divisor[buf]++;
+        }
+    }
+    return;
+}
+
+int binary_exponentiation(int a, int x){
+
+    if (x == 0) return 1;
+    else if (x%2 != 0) return a*binary_exponentiation(a, x-1);
+    else return binary_exponentiation(a, x/2)*binary_exponentiation(a, x/2);
+}
+
+
+//  main
 int main(){
 
-    ll N;
+    int N;
     cin >> N;
-    vector<ll> A;
-    map<ll, ll> dict_A;
+    vector<int> A;
+    map<int, int> dict_A;
 
-    for (ll i=0; i<N; i++){
-        ll temp;
+    for (int i=0; i<N; i++){
+        int temp;
         cin >> temp;
         dict_A[temp]++;
     }
 
-    ll ans = 0;
+    int ans = 0;
 
     for (auto p: dict_A){
 
         auto key = p.first;
         auto value = p.second;
-        ll buf = key;
 
-        if (value != 1) continue;
-
-        ll div = 2;
-        ll thr = sqrt(buf);
-        map<ll, ll> divisor;
-
-        // 素因数分解
-        if (buf == 1) {
-            ans++;
-            continue;
+        map<int, int> divisor;
+        if ((key == 1) && (value == 1)){
+            cout << 1 << endl;
+            return 0;
         }
-        else {
-            for (div=2; div<thr+1; div++){
-                while (buf%div == 0){
-                    buf /= div;
-                    divisor[div]++;
-                }
-            }
-            // flgをチェックする方法の場合、N=2*xxといった形だと素因数の数え漏れが発生する
-            if (buf != 1) {
-                divisor[buf]++;
-            }
+        else if ((key == 1) && (value != 1)){
+            cout << 0 << endl;
+            return 0;
         }
+        else if (value != 1) continue;
+        prime_factorization(key, divisor);
 
         bool flg = true;
         for (auto q: divisor){
             auto qkey = q.first;
             auto qvalue = q.second;
 
-            for (ll i=1; i<=qvalue; i++){
-                if (dict_A.count(ruijo(qkey, i))){
-                    if (ruijo(qkey, i) == key) continue;
+            for (int i=1; i<=qvalue; i++){
+                if (dict_A.count(binary_exponentiation(qkey, i))){
+                    if (binary_exponentiation(qkey, i) == key) flg = true;
                     else {
                         flg = false;
                         break;
@@ -68,21 +87,10 @@ int main(){
             if (flg == false) break;
         }
         if (flg) ans++;
+        divisor.clear();
     }
 
     cout << ans << endl;
     return 0;
 }
 
-int ruijo(int a, int b){
-
-    if (b == 0) return 1;
-
-    return a*ruijo(a, b-1);
-}
-ll ruijo(ll a, ll b){
-
-    if (b == 0) return 1;
-
-    return a*ruijo(a, b-1);
-}
