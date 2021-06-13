58792ed53b795e7d58905adf16a85249bcb83659
tsksnhr <toshiki.o3o5.s@gmail.com>
2021-06-14 02:17:50 +0900

    revised code
    add comment and implemented right sover

--- answer/abc205d.cpp
+++ answer/abc205d.cpp
@@ -1,4 +1,4 @@
-// 
+// Kth Excluded
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,15 +13,28 @@ int main(){
     ll N, Q;
     cin >> N >> Q;
     vector<ll> A(N_MAX, INF);
-    vector<ll> G_int(N_MAX, 0);
+    vector<ll> G_int(N_MAX, INF);
     for (ll i = 0; i < N; i++){
         cin >> A.at(i);
-        G_int.at(i) = A.at(i) - i;
+        G_int.at(i) = A.at(i) - (i + 1);
     }
-    sort(A.begin(), A.end());
 
     for (ll i = 0; i < Q; i++){
-        
+        ll K;
+        cin >> K;
+
+        if (G_int.at(N-1) < K){
+            ll ans = A.at(N-1) + (K - G_int.at(N-1));
+            cout << ans << endl;
+        }
+        else{
+            auto iter = lower_bound(G_int.begin(), G_int.end(), K);
+            ll pos = distance(G_int.begin(), iter);
+//            cerr << "pos = " << pos << endl;
+
+            ll ans = (A.at(pos) - 1) - (G_int.at(pos) - K);
+            cout << ans << endl;
+        }
     }
 
     return 0;
