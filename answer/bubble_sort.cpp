11e3d1d937fcdf412739e35097de00db2e6bb5f4
tsksnhr <toshiki.o3o5.s@gmail.com>
2021-06-16 21:13:30 +0900

    create file
    learned bubble-sort

---
+++ answer/bubble_sort.cpp
@@ -0,0 +1,52 @@
+// Bubble Sort
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void outputarray(vector<int> &A){
+
+    for (int i = 0; i < A.size(); i++){
+        cout << A.at(i);
+        if (i != A.size() - 1) cout << " ";
+        else cout << endl;
+    }
+
+    return;
+}
+
+int bubble_sort(int N, vector<int> &A){
+
+    int cnt = 0;
+    bool is_OK = 0;
+
+    while (!is_OK){
+        is_OK = 1;
+
+        for (int i = N-1; i > 0; i--){
+            if (A.at(i-1) > A.at(i)){
+                int tmp = A.at(i);
+                A.at(i) = A.at(i-1);
+                A.at(i-1) = tmp; 
+                is_OK = 0;
+                cnt++;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+int main(){
+
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) cin >> A.at(i);
+
+    int cnt = bubble_sort(N, A);
+
+    outputarray(A);
+    cout << cnt << endl;
+
+    return 0;
+}
