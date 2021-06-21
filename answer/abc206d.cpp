1d1f8f5c895df903db38c94d8a89e9fbfd6a09bf
tsksnhr <toshiki.o3o5.s@gmail.com>
2021-06-21 17:26:42 +0900

    implemented right solver

--- answer/abc206d.cpp
+++ answer/abc206d.cpp
@@ -4,39 +4,68 @@
 using namespace std;
 using ll = long long;
 
-// TODO: solve
+void dfs(vector<vector<int>> &graph, vector<bool> &used, int now){
+
+    used.at(now) = true;
+
+    for (auto next: graph.at(now)){
+        if (used.at(next) == true) continue;
+        dfs(graph, used, next);
+    }
+
+    return;
+}
 
 int main(){
     
-    ll N;
+    int N;
     cin >> N;
-    vector<ll> A(N);
-    for (ll i = 0; i < N; i++) cin >> A.at(i);
+    vector<int> A(N);
+    set<int> A_set;
+    int A_max = 0;
+    for (int i = 0; i < N; i++){
+        cin >> A.at(i);
+        A_max = max(A_max, A.at(i));
+        A_set.insert(A.at(i));
+    }
+    int A_kind = A_set.size();
 
-    vector<ll> B(N);
+    vector<int> B(N);
     B = A;
     reverse(B.begin(), B.end());
     if (A == B){
         cout << 0 << endl;
         return 0;
     }
-    
-    vector<ll> dif(N);
-    map<ll, ll> dif_dict;
-    for (ll i = 0; i < N; i++){
-        if (A.at(i) != B.at(i)) dif_dict[A.at(i)]++;
+
+    // max-size fo this graph is not N because a number in A may not be N.
+    vector<vector<int>> graph(A_max);
+    for (int i = 0; i < N; i++){
+        int a, b;
+        a = A.at(i);
+        b = B.at(i);
+        if (a != b){
+            a--;
+            b--;
+            graph.at(a).push_back(b);
+            graph.at(b).push_back(a);
+        }
     }
 
-    ll total = 0;
-    for (auto p: dif_dict){
-        total++;
-    }
-    for (auto p: dif_dict){
-        dif_dict.count();
+    int cnt = 0;
+    // max-size fo this array is not N because a number in A may not be N. 
+    vector<bool> used(A_max, false);
+    for (auto x: A_set){
+        auto start = x;
+        start--;
+
+        if (used.at(start) == false){
+            dfs(graph, used, start);
+            cnt++;
+        }
     }
 
-    ll ans = total/2;
-    if (N%2 == 0) ans++;
+    int ans = A_kind - cnt;
     cout << ans << endl;
 
     return 0;
