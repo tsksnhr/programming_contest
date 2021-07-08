8f038b96df74b7b24aef8a07f0d02f275accf431
tsksnhr <toshiki.o3o5.s@gmail.com>
2021-07-08 23:18:13 +0900

    create file
    solved problem

--- answer/ALDS1_8_A.cpp
+++ answer/ALDS1_8_A.cpp
@@ -19,11 +19,12 @@ typedef struct NodeData{
 
 Node *root, *NIL;
 
-void Insert(Node *tree, ll key){
+void Insert(ll key){
 
     Node *x;
     Node *y;
     Node *z;
+//    cerr << x << " " << y << " " << z << endl;
 
     x = root;
     y = NIL;
@@ -32,16 +33,69 @@ void Insert(Node *tree, ll key){
     z->left = NIL;
     z->right = NIL;
     z->parent = NIL;
+//    cerr << x << " " << y << " " << z << endl;
 
     while (x != NIL){
         y = x;
         if (key > x->key) x = x->right;
         else x = x->left;
     }
-    x->key = key;
+    z->parent = y;
 
+    if (y == NIL) root = z;
+    else if (key > y->key) y->right = z;
+    else y->left = z;
+
+    return;
+}
+
+void PreOrder(Node *node){
+    
+    cout << " " << node->key;
+//    cerr << "pre" << endl;
+    
+    if (node->left != NIL) PreOrder(node->left);
+    if (node->right != NIL) PreOrder(node->right);
+    
+    return;
+}
+
+void InOrder(Node *node){
+    
+    if (node->left != NIL) InOrder(node->left);
+    cout << " " << node->key;
+//    cerr << "in" << endl;
+
+    if (node->right != NIL) InOrder(node->right);
+    
+    return;
 }
 
 int main(){
 
+//    Initialize();
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++){
+//        cerr << i << endl;
+        string command;
+        ll key;
+        cin >> command;
+
+        if (command == "insert"){
+            cin >> key;
+//            cerr << "inserted" << endl;
+            Insert(key);
+        }
+        else{
+            InOrder(root);
+            cout << endl;
+            PreOrder(root);
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
